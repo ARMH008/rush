df09c8d6bc35f986c92d7e95bfb5ddec767b2330
ARMH008 <158465673+ARMH008@users.noreply.github.com>
2024-11-30 15:53:28 +0530

    Create axios.js

---
+++ view/src/lib/axios.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+export const axiosInstance = axios.create({
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:3000/api/v1"
+      : "/api/v1",
+  withCredentials: true,
+});
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
