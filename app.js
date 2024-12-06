909730abbbe4b96738e3dce18664ea30e059fb55
ARMH008 <158465673+ARMH008@users.noreply.github.com>
2024-12-06 13:41:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const cors = require("cors");
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
-app.use(express.json());
+
 if (process.env.NODE_ENV !== "production") {
   app.use(
     cors({
