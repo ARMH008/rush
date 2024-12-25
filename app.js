f75b79e8ff8e45fc2adff2bc1b34abfb33137c58
ARMH008 <158465673+ARMH008@users.noreply.github.com>
2024-12-25 19:51:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -26,49 +26,54 @@ const cors = require("cors");
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
-
-/* if (process.env.NODE_ENV !== "production") {
-  app.use(
-    cors({
-    origin: "https://rush-construction-frontend.onrender.com",
-      credentials: true,
-    })
-  );
-} */
+app.use(express.json());
 //app.use(express.urlencoded({ extended: true }));
 app.use(cookieparser());
-app.use(
-  cors({
-     origin: "https://rush-construction-frontend.onrender.com",
-    credentials: true,
-  
-    optionsSuccessStatus: 200,
-    allowedHeaders: [
-      "set-cookie",
-      "Content-Type",
-      "Access-Control-Allow-Origin",
-      "Access-Control-Allow-Credentials",
-    ],
-  })
-);
+
+if (process.env.NODE_ENV === "development") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+      credentials: true,
+      origin: true,
+      optionsSuccessStatus: 200,
+      allowedHeaders: [
+        "set-cookie",
+        "Content-Type",
+        "Access-Control-Allow-Origin",
+        "Access-Control-Allow-Credentials",
+      ],
+    })
+  );
+}
+
 app.use(cookieparser());
 
 //data sanitization against NoSQL query injection
 app.use(monogsantize());
 //Data sanitization--malicious html code
 app.use(xss());
-
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/sitereport", siteRouter);
 
-app.all("*", (req, res, next) => {
-  next(new AppError(`can't find the ${req.originalUrl} url`));
-});
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "view/dist")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "view/dist")));
 
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "view", "dist", "index.html"));
+//   });
+// }
+if (process.env.NODE_ENV === "production") {
+  // Serve static files from the React build directory
+  app.use(express.static(path.join(__dirname, "view/dist")));
+  app.use("/assets", express.static(path.join(__dirname, "view/dist/assets")));
+
+  // Handle React routing, return all requests to React app
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "view", "dist", "index.html"));
   });
 }
+app.all("*", (req, res, next) => {
+  next(new AppError(`can't find the ${req.originalUrl} url`));
+});
 module.exports = app;
