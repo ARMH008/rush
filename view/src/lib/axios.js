2db84fdd586fe97ce0c0a0e1704e2cbe9f8d889f
ARMH008 <158465673+ARMH008@users.noreply.github.com>
2024-12-25 19:40:43 +0530

    Update axios.js

--- view/src/lib/axios.js
+++ view/src/lib/axios.js
@@ -4,7 +4,7 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:3000/api/v1"
-      : "https://rush-construction-backend.onrender.com/api/v1",  // Live production API
+      : "/api/v1",  // Live production API
   withCredentials: true,
 });
 //   headers: {
